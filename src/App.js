fae3d54036fda6d42a5afc58ee29c609c0a732f5
hayatul-islam <hayatulemon340@gmail.com>
2022-09-15 20:54:02 +0600

    insurance design

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from "react";
-import { BrowserRouter, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "./components/common/Header";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Header />
-        <Routes>{/* <Route path="/" element={} /> */}</Routes>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
       </BrowserRouter>
     </>
   );
---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Insurance from "../components/Home/Insurance";
+
+export default function Home() {
+  return (
+    <>
+      <div className="px-6">
+        <Insurance />
+      </div>
+    </>
+  );
+}
