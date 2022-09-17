6dfedb636d4b65c80313004ac92cea264970d298
hayatul-islam <hayatulemon340@gmail.com>
2022-09-17 13:11:47 +0600

    product button animation done

--- src/components/Home/Product.js
+++ src/components/Home/Product.js
@@ -1,24 +1,37 @@
-import React from "react";
+import { useInView } from "framer-motion";
+import React, { useEffect, useRef } from "react";
 
 function Product({ product, singelProductId, setSingleProductId }) {
   const { title, details, productId } = product;
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: false });
+
+  useEffect(() => {
+    if (isInView) {
+      setSingleProductId(productId);
+    } else {
+      setSingleProductId(productId);
+    }
+  }, [isInView, productId]);
 
   return (
-    <div className="flex space-x-12">
-      <div className="min-w-[70px] mt-1">
-        <button
-          onClick={() => setSingleProductId(productId)}
-          className={`text-sm border border-black px-[5px] py-px rounded-lg ${
-            singelProductId === productId && "bg-[#20231f] text-white"
-          }`}
-        >
-          {title}
-        </button>
+    <>
+      <div ref={ref} id={productId} className="flex space-x-12">
+        <div className="min-w-[70px] mt-1">
+          <button
+            onClick={() => setSingleProductId(productId)}
+            className={`text-sm border border-black px-[5px] py-px rounded-lg ${
+              singelProductId === productId && "bg-[#20231f] text-white"
+            }`}
+          >
+            {title}
+          </button>
+        </div>
+        <div>
+          <p className="max-w-[474px] text-[16px]">{details}</p>
+        </div>
       </div>
-      <div>
-        <p className="max-w-[474px] text-[16px]">{details}</p>
-      </div>
-    </div>
+    </>
   );
 }
 
