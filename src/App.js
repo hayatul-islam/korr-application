e9bba88df39408241469f21a0e000b8c8cdeb442
hayatul-islam <hayatulemon340@gmail.com>
2022-09-18 11:14:57 +0600

    code stracture

--- src/App.js
+++ src/App.js
@@ -1,19 +1,10 @@
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Footer from "./components/common/Footer";
-import Header from "./components/common/Header";
-import Home from "./pages/Home";
+import TheLayout from "./pages/TheLayout";
 
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-        <Footer />
-      </BrowserRouter>
+      <TheLayout />
     </>
   );
 }
---
+++ src/pages/TheLayout.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Footer from "../components/common/Footer";
+import Header from "../components/common/Header";
+import Home from "./Home";
+
+export default function TheLayout() {
+  return (
+    <>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </>
+  );
+}
