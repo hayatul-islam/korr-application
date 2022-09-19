8e4a7ae683ba52b04cf4d1fe90af9063c31671be
hayatul-islam <hayatulemon340@gmail.com>
2022-09-19 16:54:25 +0600

    framer motion animation add

--- src/components/Home/VideoAnimation.js
+++ src/components/Home/VideoAnimation.js
@@ -1,19 +1,31 @@
+import { motion } from "framer-motion";
 import React from "react";
 import videoAnimation from "../../assets/video/home_video.mp4";
 
 function VideoAnimation() {
   return (
     <>
-      <div className="md:px-6 my-4">
-        <video
-          className=" lg:h-[600px] w-full md:rounded-2xl object-cover "
-          autoPlay
-          muted
-          loop
-        >
-          <source src={videoAnimation} type="video/mp4" />
-        </video>
-      </div>
+      <motion.div
+        className="box"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{
+          duration: 1,
+          delay: 0.8,
+          ease: [0, 0.71, 0.2, 1.01],
+        }}
+      >
+        <div className="md:px-6 my-4">
+          <video
+            className=" lg:h-[600px] w-full md:rounded-2xl object-cover "
+            autoPlay
+            muted
+            loop
+          >
+            <source src={videoAnimation} type="video/mp4" />
+          </video>
+        </div>
+      </motion.div>
     </>
   );
 }
