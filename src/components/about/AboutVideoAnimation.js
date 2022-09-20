e08f872b332ddccf1eed46fa0feaee70b22aceb2
hayatul-islam <hayatulemon340@gmail.com>
2022-09-21 00:15:09 +0600

    about video animation design

---
+++ src/components/about/AboutVideoAnimation.js
@@ -0,0 +1,13 @@
+import React from "react";
+import sun_light from "../../assets/video/sun_light.mp4";
+import VideoAnimation from "../common/VideoAnimation";
+
+function AboutVideoAnimation() {
+  return (
+    <>
+      <VideoAnimation video={sun_light} />
+    </>
+  );
+}
+
+export default AboutVideoAnimation;
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,5 +1,6 @@
 import React from "react";
 import AboutTitle from "../components/about/AboutTitle";
+import AboutVideoAnimation from "../components/about/AboutVideoAnimation";
 
 function About() {
   return (
@@ -7,6 +8,7 @@ function About() {
       <div className="px-6">
         <AboutTitle />
       </div>
+      <AboutVideoAnimation />
     </>
   );
 }
