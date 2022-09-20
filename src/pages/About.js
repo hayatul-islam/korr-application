21e3a382bada6da8f6ffc7592232a69b81bb61ce
hayatul-islam <hayatulemon340@gmail.com>
2022-09-21 00:10:37 +0600

    about title design

---
+++ src/pages/About.js
@@ -0,0 +1,14 @@
+import React from "react";
+import AboutTitle from "../components/about/AboutTitle";
+
+function About() {
+  return (
+    <>
+      <div className="px-6">
+        <AboutTitle />
+      </div>
+    </>
+  );
+}
+
+export default About;
--- src/pages/TheLayout.js
+++ src/pages/TheLayout.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Footer from "../components/common/Footer";
 import Header from "../components/common/header/Header";
+import About from "./About";
 import BookADemo from "./BookADemo";
 import Home from "./Home";
 import Product from "./Product";
@@ -15,6 +16,7 @@ export default function TheLayout() {
           <Route path="/" element={<Home />} />
           <Route path="/product" element={<Product />} />
           <Route path="/book-a-demo" element={<BookADemo />} />
+          <Route path="/about" element={<About />} />
         </Routes>
         <Footer />
       </BrowserRouter>
